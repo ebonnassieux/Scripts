058db1dc5a82747b2356112d2afc9722860ac7e3
ebonnassieux <etienne.bonnassieux@obspm.fr>
2017-07-17 16:57:29 +0200

    wgw

--- PngCutoutFromFits.py
+++ PngCutoutFromFits.py
@@ -6,7 +6,7 @@
 import argparse
 import matplotlib
 matplotlib.use("Agg")
-#import aplpy
+import aplpy
 import numpy as np
 import sys
 import glob
@@ -17,22 +17,24 @@ warnings.filterwarnings("ignore")
 def readArguments():
     # create parser
     parser=argparse.ArgumentParser(description="Make a png cutout from a FITS image.")
-    parser.add_argument("--filename",type=str,help="Name of the .fits file you want a cutout of",required=True,nargs=argparse.REMAINDER)
+    parser.add_argument("-v","--verbose",help="Be verbose, say everything program does. Default is True",required=False,action="store_true")
+    parser.add_argument("--filename",type=str,help="Name of the .fits file you want a cutout of",required=True,nargs="+")
     parser.add_argument("--RA",metavar="HH:MM:SS",type=str, help="Right Ascension in hexadecimal hour angle",required=True)
     parser.add_argument("--Dec",metavar="HH:MM:SS",type=str,help="Declination in hexadecimal degrees",required=True)
     parser.add_argument("--vmin",type=str,default=-2,help="Minimum value for display, in units of rms. Default -2.",required=False)
     parser.add_argument("--vmax",type=str,default=40,help="Maximum value for display, in units of rms. Default 40.",required=False)
     parser.add_argument("--size",metavar="arcmin",type=float,default=5,help="Size of cutout, in arcmin. Default is 5")
+    parser.add_argument("--noise",metavar="all/first",choices=["all","first"],type=str,default="first",help="Set of images over which to estimate noise. Default is \"first\", only the first image in the list.",required=False)
     # parse
     args=parser.parse_args()
     return vars(args)
 
 
-def MakeCutout(fitsfiles,RA,Dec,xDegrees=0.1,yDegrees=0.1,NSigmaVmax=10,NSigmaVmin=-10,outname=None,SetVminToAverageNoise=False):
+def MakeCutout(fitsfiles,RA,Dec,xDegrees=0.1,yDegrees=0.1,NSigmaVmax=10,NSigmaVmin=-10,outname=None,SetVminToAverageNoise="first",verb=0):
     RAdeg  = HHMMSStoDegrees(RA)*15. # this converts RA from hours to degrees
     Decdeg = HHMMSStoDegrees(Dec)
     # find vmin, vmax
-    if SetVminToAverageNoise==True:
+    if SetVminToAverageNoise=="all":
         stdarray=[]
         for ffile in fitsfiles:
             im=image(ffile)
@@ -49,7 +51,7 @@ def MakeCutout(fitsfiles,RA,Dec,xDegrees=0.1,yDegrees=0.1,NSigmaVmax=10,NSigmaVm
         vmax=NSigmaVmax*np.mean(stdarray)        
     else:
         # calculate noise in first image, use that as a reference
-        print "Calculating noise in %s, use as reference for all images"%fitsfiles[0]
+        if verb: print "Calculating noise in %s, use as reference for all images"%fitsfiles[0]
         im=image(fitsfiles[0])
         d=im.getdata()
         ind=np.int64(np.random.rand(10000)*d.size)
@@ -86,21 +88,16 @@ def HHMMSStoDegrees(HHMMSS):
 
 if __name__=="__main__":
     args=readArguments()
-    print args
     fitsfiles=args["filename"]
     RA=args["RA"]
-    print fitsfiles
-    print RA
-
-    #fitsfiles=sys.argv[1:-2]
-    #print "List of .fits: ",fitsfiles
-    #RA=sys.argv[-2]
-    #Dec=sys.argv[-1]
-    #print "RA  : %s"%RA
-    #print "Dec : %s"%Dec
-    #vmin=-1  # in units of sigma
-    #vmax= 1.2  # in units of sigma 
-
-    # make some control loop to say that RA, Dec need to be given in hexadecimal
-    
-    #MakeCutout(fitsfiles,RA,Dec,xDegrees=4,yDegrees=4,NSigmaVmin=vmin,NSigmaVmax=vmax,outname=None)
+    Dec=args["Dec"]
+    vmin=args["vmin"]
+    vmax=args["vmax"]
+    size=args["size"]
+    verb=args["verbose"]
+    noisearg=args["noise"]
+    if verb:
+        print "Making cutouts of size %2.1f' at (RA=%s,Dec=%s) for the following files:"%(size,RA,Dec)
+        for i in fitsfiles:
+            print i
+    MakeCutout(fitsfiles,RA,Dec,xDegrees=size,yDegrees=size,NSigmaVmin=vmin,NSigmaVmax=vmax,outname=None,verb=verb,SetVminToAverageNoise=noisearg)
