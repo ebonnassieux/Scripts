3a21ee3b225cf92b6e53a54dac6147848fe481f9
ebonnassieux <etienne.bonnassieux@obspm.fr>
2017-08-10 12:26:13 +0000

    asqw

--- CovWeights.doesfullweightsSomehow.py
+++ CovWeights.doesfullweightsSomehow.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from pyrap.tables import table
 import numpy as np
@@ -12,14 +13,15 @@ def readArguments():
     parser=argparse.ArgumentParser("Calculate visibility imagin weights based on calibration quality")
     parser.add_argument("-v","--verbose",help="Be verbose, say everything program does. Default is False",required=False,action="store_true")
     parser.add_argument("--filename",type=str,help="Name of the measurement set for which weights want to be calculated",required=True,nargs="+")
-    parser.add_argument("--ntsol",type=float,help="Solution interval, in timesteps, for your calibration",required=True)
+    parser.add_argument("--ntsol",type=int,help="Solution interval, in timesteps, for your calibration",required=True)
     parser.add_argument("--lambda",type=int,help="Determines how many neighbours covariance is calculated for: default is 0",default=0,required=False)
+    parser.add_argument("--noisemap",help="Save dudv data + covariances in measurement set",required=False,action="store_true")
     args=parser.parse_args()
     return vars(args)
 
 
 class CovWeights:
-    def __init__(self,MSName,ntsol=1,MaxCorrTime=0,SaveDataProducts=0):
+    def __init__(self,MSName,ntsol=1,MaxCorrTime=0,SaveDataProducts=True):
         if MSName[-1]=="/":
             self.MSName=MSName[0:-1]
         else:
@@ -31,9 +33,9 @@ class CovWeights:
     def FindWeights(self,tcorr=0):
         #if tcorr!=0:
         #    tcorr=tcorr*self.ntSol
-        ms=table(self.MSName,readonly=False)
+        ms=table(self.MSName,readonly=False,ack=verb)
         # open antennas
-        ants=table(ms.getkeyword("ANTENNA"))
+        ants=table(ms.getkeyword("ANTENNA"),ack=verb)
         # open antenna tables
         antnames=ants.getcol("NAME")
         nAnt=len(antnames)
@@ -44,12 +46,12 @@ class CovWeights:
         nbl=np.where(Times==Times[0])[0].size
         warnings.filterwarnings("ignore")
         # TODO: find better name
-        #norm=1/np.ones_like(ms.getcol("CORRECTED_DATA"))
+        #norm=1/np.ones_like(ms.getcol("MODEL_DATA"))
         warnings.filterwarnings("default")
         # get rid of NaN
         #norm[np.isnan(norm)]=0
         if "RESIDUAL_DATA" not in ms.colnames():
-            print "RESIDUAL_DATA not in MS: adding and filling with CORRECTED_DATA"
+            if verb: print "RESIDUAL_DATA not in MS: adding and filling with CORRECTED_DATA"
             desc=ms.getcoldesc("CORRECTED_DATA")
             desc["name"]="RESIDUAL_DATA"
             desc['comment']=desc['comment'].replace(" ","_")
@@ -69,7 +71,6 @@ class CovWeights:
         nPola=residualdata.shape[2]
         nt=residualdata.shape[0]/nbl
         # reshape antennas and data columns
-        print residualdata.shape,nt,nbl,nChan,nPola
         residualdata=residualdata.reshape((nt,nbl,nChan,nPola))
         # average residual data within calibration cells
         if self.ntSol>1:
@@ -94,14 +95,10 @@ class CovWeights:
         # antenna coefficient array
         residuals=residuals-np.mean(residuals)
         # ensure things are properly flagged
-        #residuals[np.isnan(residuals)==1]=0
-        # TODO - MAKE COEFFARRAY INTO COMPLEX-VALUED WEIGHTS
-        CoeffArray=np.zeros((nt,nAnt))#,dtype=np.complex64)
-        # start calculating the weights
-        print "Begin calculating antenna-based coefficients"
+        CoeffArray=np.zeros((nt,nAnt))        # start calculating the weights
+        if verb: print "Begin calculating antenna-based coefficients"
         if tcorr>1:
             ### SAVE DATA ###
-
             colname="COV_WEIGHT"
             ms=table(self.MSName,readonly=False)
             # open antennas
@@ -118,7 +115,7 @@ class CovWeights:
             A0=np.array(ms.getcol("ANTENNA1").reshape((nt,nbl)))
             A1=np.array(ms.getcol("ANTENNA2").reshape((nt,nbl)))
             if colname in ms.colnames():
-                print "%s column already present; will overwrite"%colname
+                if verb: print "%s column already present; will overwrite"%colname
             else:
                 W=np.ones((nt*nbl,nchan))
                 desc=ms.getcoldesc("WEIGHT_SPECTRUM")
@@ -129,15 +126,16 @@ class CovWeights:
             # create weight array
             w=np.zeros((nt,nbl,nchan))
             ant1=np.arange(nAnt)
-            print "Fill weights array"
+            if verb: print "Fill weights array"
             A0ind=A0[0,:]
             A1ind=A1[0,:]
 
             ### WEIGHT COL CREATED ###
 
             warnings.filterwarnings("ignore")
-            print "find covariance between %i nearest times"%tcorr
+            if verb: print "find covariance between %i nearest times"%tcorr
             for ant1 in A0:
+                print ant1,time.time()
                 for ant2 in A1:
                     line=0
                     #print "Antenna %i of %i"%(ant,nAnt)
@@ -167,11 +165,11 @@ class CovWeights:
                             line=line+1
                         elif t_hi==0:
                             line=line-1
-                PrintProgress(ant1,nAnt)
+                    if verb: PrintProgress(ant2,nAnt)
             warnings.filterwarnings("default")
         else:
             warnings.filterwarnings("ignore")
-            print "Find variance-only weights"
+            if verb: print "Find variance-only weights"
             for t_i in range(nt):
                 # build weights for each antenna at time t_i
                 for ant in ant1:
@@ -193,22 +191,22 @@ class CovWeights:
                                            - np.std( (np.append(rmsarray[t_i,set1,:,:], rmsarray[t_i,set2,:,:]))) )
 #                    CoeffArray[t_i,ant] = (np.mean(np.sqrt(np.append(residuals[t_i,set1,:,:],residuals[t_i,set2,:,:])*np.append(residuals[t_i,set1,:,:],residuals[t_i,set2,:,:]).conj())\
 #                                           - np.std( (np.append(rmsarray[t_i,set1,:,:], rmsarray[t_i,set2,:,:])))) )
-                PrintProgress(t_i,nt)
+                if verb: PrintProgress(t_i,nt)
         warnings.filterwarnings("default")
 #        CoeffArray=CoeffArray-np.mean(CoeffArray)/3.
         for i in range(nAnt):
             thres=0.25*np.median(CoeffArray[:,i])
             CoeffArray[CoeffArray[:,i]<thres,i]=thres
-        coeffFilename="%s/CoeffArray.%i.no1stant.npy"%(msname,tcorr)
-        print "Save coefficient array as %s."%coeffFilename
+        coeffFilename="%s/CoeffArray.%i.no1stant.npy"%(self.MSName,tcorr)
+        if verb: print "Save coefficient array as %s."%coeffFilename
         np.save(coeffFilename,CoeffArray)
         return CoeffArray
                         
     def SaveWeights(self,CoeffArray,colname="VAR_WEIGHT",AverageOverChannels=True,tcorr=0,PreserveResolution=True):
-        print "Begin saving the data"
-        ms=table(self.MSName,readonly=False)
+        if verb: print "Begin saving the data"
+        ms=table(self.MSName,readonly=False,ack=verb)
         # open antennas
-        ants=table(ms.getkeyword("ANTENNA"))
+        ants=table(ms.getkeyword("ANTENNA"),ack=verb)
         # open antenna tables
         antnames=ants.getcol("NAME")
         nAnt=len(antnames)
@@ -221,7 +219,7 @@ class CovWeights:
         A0=np.array(ms.getcol("ANTENNA1").reshape((nt,nbl)))
         A1=np.array(ms.getcol("ANTENNA2").reshape((nt,nbl)))
         if colname in ms.colnames():
-            print "%s column already present; will overwrite"%colname
+            if verb: print "%s column already present; will overwrite"%colname
         else:
             W=np.ones((nt*nbl,nchan))
             desc=ms.getcoldesc("IMAGING_WEIGHT")
@@ -232,7 +230,7 @@ class CovWeights:
         # create weight array
         w=np.zeros((nt,nbl,nchan))
         ant1=np.arange(nAnt)
-        print "Fill weights array"
+        if verb: print "Fill weights array"
         A0ind=A0[0,:]
         A1ind=A1[0,:]
         
@@ -242,7 +240,7 @@ class CovWeights:
             for j in range(nchan):
                 w[:,i,j]=1./(CoeffArray[:,A0ind[i]] + CoeffArray[:,A1ind[i]] + 0.01 + np.sqrt(CoeffArray[:,A0ind[i]]*CoeffArray[:,A1ind[i]]))
                 #w[:,i,j]=1./(CoeffArray[:,A0ind[i]]**2 + CoeffArray[:,A1ind[i]]**2 + 0.01)
-            PrintProgress(i,nbl)
+            if verb: PrintProgress(i,nbl)
         warnings.filterwarnings("default")
         w[np.isnan(w)]=0
         w[np.isinf(w)]=0
@@ -285,9 +283,11 @@ if __name__=="__main__":
     msname      = args["filename"]
     corrtime    = args["lambda"]
     ntsol       = args["ntsol"]
+    global verb
+    verb        = args["verbose"]
     for ms in msname:
-        print "Finding time-covariance weights for: %s"%ms
+        if verb: print "Finding time-covariance weights for: %s"%ms
         covweights=CovWeights(MSName=ms,ntsol=ntsol)
         coefficients=covweights.FindWeights(tcorr=corrtime)
         covweights.SaveWeights(coefficients,colname="VAR_WEIGHT",tcorr=corrtime)
-    print "Total runtime: %f min"%((time.time()-start_time)/60.)
+    if verb: print "Total runtime: %f min"%((time.time()-start_time)/60.)
