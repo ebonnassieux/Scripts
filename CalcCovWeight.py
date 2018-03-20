be5e9b07061faedc6db7d07c7eb42f20c8d5e266
ebonnassieux <etienne.bonnassieux@obspm.fr>
2018-03-20 10:44:17 +0000

    asdadas

---
+++ CalcCovWeight.py
@@ -0,0 +1,184 @@
+import os
+from pyrap.tables import table
+import numpy as np
+import pylab
+from numpy import ma
+import sys
+import warnings
+import time
+import math
+import argparse
+
+class CovWeights:
+    def __init__(self,MSName,ntsol=1,SaveDataProducts=0):
+        if MSName[-1]=="/":
+            self.MSName=MSName[0:-1]
+        else:
+            self.MSName=MSName
+        self.MaxCorrTime=0
+        self.SaveDataProducts=SaveDataProducts
+        self.ntSol=ntsol
+
+    def FindWeights(self,tcorr=0):
+        ms=table(self.MSName)
+        # open antennas
+        ants=table(ms.getkeyword("ANTENNA"))
+        # open antenna tables
+        antnames=ants.getcol("NAME")
+        nAnt=len(antnames)
+        # load ant indices
+        A0=ms.getcol("ANTENNA1")
+        A1=ms.getcol("ANTENNA2")
+        Times=ms.getcol("TIME")
+        nbl=np.where(Times==Times[0])[0].size
+        warnings.filterwarnings("ignore")
+        warnings.filterwarnings("default")
+        if "RESIDUAL_DATA" not in ms.colnames():
+            desc=ms.getcoldesc("CORRECTED_DATA")
+            desc["name"]="RESIDUAL_DATA"
+            desc['comment']=desc['comment'].replace(" ","_")
+            ms.addcols(desc)
+            ms.putcol("RESIDUAL_DATA",ms.getcol("CORRECTED_DATA"))
+        flags=ms.getcol("FLAG")
+        print "Please ensure that RESIDUAL_DATA or CORRECTED_DATA contains residual visibilities from complete skymodel subtraction, and PREDICTED_VIS contains the uncalibrated flux."
+        residualdata=ms.getcol("RESIDUAL_DATA")        
+        flags=ms.getcol("FLAG")
+        # apply flags to data
+        residualdata[flags==1]=0
+        # exit files gracefully
+        ants.close()
+        # initialise
+        nChan=residualdata.shape[1]
+        nPola=residualdata.shape[2]
+        nt=residualdata.shape[0]/nbl
+        # reshape antennas and data columns
+        residualdata=residualdata.reshape((nt,nbl,nChan,nPola))
+        # average residual data within calibration cells
+        if self.ntSol>1:
+            tspill=nt%self.ntSol
+            nt1=nt+self.ntSol-tspill
+            for i in range(nt1/self.ntSol):
+                residualdata[i*self.ntSol:(i+1)*self.ntSol,:,:,:]=np.mean(residualdata[i*self.ntSol:(i+1)*self.ntSol,:,:,:],axis=0)
+        A0=A0.reshape((nt,nbl))
+        A1=A1.reshape((nt,nbl))
+        ant1=np.arange(nAnt)
+        # make rms array
+        darray=ms.getcol("CORRECTED_DATA").reshape((nt,nbl,nChan,nPola))
+        ms.close()
+        rmsarray=np.zeros((nt,nbl,nChan,2),dtype=np.complex64)
+        residuals=np.zeros_like(rmsarray,dtype=np.complex64)
+        rmsarray[:,:,:,0]=darray[:,:,:,1]
+        rmsarray[:,:,:,1]=darray[:,:,:,2]
+        # make proper residual array
+        residuals[:,:,:,0]=darray[:,:,:,0]
+        residuals[:,:,:,1]=darray[:,:,:,3]
+        # antenna coefficient array
+        CoeffArray=np.zeros((nt,nAnt))
+        # start calculating the weights
+        print "Begin calculating antenna-based coefficients"
+        warnings.filterwarnings("ignore")
+        print "Find variance-only weights"
+        for t_i in range(nt):
+            # build weights for each antenna at time t_i
+            for ant in ant1:
+                # set of vis for baselines ant-ant_i
+                set1=np.where(A0[t_i]==ant1)[0]
+                # set of vis for baselines ant_i-ant
+                set2=np.where(A1[t_i]==ant)[0]
+                CoeffArray[t_i,ant] = np.sqrt(np.mean(np.append(residuals[t_i,set1,:,:],residuals[t_i,set2,:,:])*np.append(residuals[t_i,set1,:,:],residuals[t_i,set2,:,:]).conj())\
+                                              - np.std( (np.append(rmsarray[t_i,set1,:,:], rmsarray[t_i,set2,:,:]))) )
+            PrintProgress(t_i,nt)
+        warnings.filterwarnings("default")
+        for i in range(nAnt):
+            thres=0.25*np.median(CoeffArray[:,i])
+            CoeffArray[CoeffArray[:,i]<thres,i]=thres
+        coeffFilename=self.MSName+"/CoeffArray.%i.npy"%tcorr
+        print "Save coefficient array as %s"%coeffFilename
+        np.save(coeffFilename,CoeffArray)
+        return CoeffArray
+                        
+    def SaveWeights(self,CoeffArray,colname="TEST_WEIGHT_DONTUSE",AverageOverChannels=True,tcorr=0):
+        print "Begin saving the data"
+        ms=table(self.MSName,readonly=False)
+        # open antennas
+        ants=table(ms.getkeyword("ANTENNA"))
+        # open antenna tables
+        antnames=ants.getcol("NAME")
+        nAnt=len(antnames)
+        tarray=ms.getcol("TIME")
+        darray=ms.getcol("DATA")
+        tvalues=np.array(sorted(list(set(tarray))))
+        nt=tvalues.shape[0]
+        nbl=tarray.shape[0]/nt
+        nchan=darray.shape[1]
+        A0=np.array(ms.getcol("ANTENNA1").reshape((nt,nbl)))
+        A1=np.array(ms.getcol("ANTENNA2").reshape((nt,nbl)))
+        if colname in ms.colnames():
+            print "%s column already present; will overwrite"%colname
+        else:
+            W=np.ones((nt*nbl,nchan))
+            desc=ms.getcoldesc("IMAGING_WEIGHT")
+            desc["name"]=colname
+            desc['comment']=desc['comment'].replace(" ","_")
+            ms.addcols(desc)
+            ms.putcol(colname,W)
+        # create weight array
+        w=np.zeros((nt,nbl,nchan))
+        ant1=np.arange(nAnt)
+        print "Fill weights array"
+        A0ind=A0[0,:]
+        A1ind=A1[0,:]
+        warnings.filterwarnings("ignore")
+        for i in range(nbl):
+            for j in range(nchan):
+                w[:,i,j]=1./(CoeffArray[:,A0ind[i]]*CoeffArray[:,A1ind[i]] + 0.01)
+            PrintProgress(i,nbl)
+        warnings.filterwarnings("default")
+        w=w.reshape(nt*nbl,nchan)
+        w[np.isnan(w)]=0
+        w[np.isinf(w)]=0
+        # normalise
+        w=w/np.mean(w)
+        # save in weights column
+        ms.putcol(colname,w)
+        ants.close()
+        ms.close()
+
+### auxiliary functions ###
+def PrintProgress(currentIter,maxIter,msg=""):
+    sys.stdout.flush()
+    if msg=="":
+        msg="Progress:"
+    sys.stdout.write("\r%s %5.1f %% "%(msg,100*(currentIter+1.)/maxIter))
+    if currentIter==(maxIter-1):
+        sys.stdout.write("\n")
+def invSVD(A):
+    u,s,v=np.linalg.svd(A)
+    s[s<1.e-6*s.max()]=1.e-6*s.max()
+    ssq=np.abs((1./s))
+    # rebuild matrix
+    Asq=np.dot(v,np.dot(np.diag(ssq),np.conj(u)))
+    v0=v.T*ssq.reshape(1,ssq.size)
+    return Asq
+def readArguments():
+    parser=argparse.ArgumentParser("Calculate visibility imagin weights based on calibration quality")
+    parser.add_argument("-v","--verbose",help="Be verbose, say everything program does. Default is False",required=False,action="store_true")
+    parser.add_argument("--filename",type=str,help="Name of the measurement set for which weights want to be calculated",required=True,nargs="+")
+    parser.add_argument("--ntsol",type=int,help="Solution interval, in timesteps, for your calibration",required=True)
+#    parser.add_argument("--nchansol",type=int,help="Solution interval, in channels, for your calibration",required=True)
+    args=parser.parse_args()
+    return vars(args)
+
+
+
+### if program is called as main ###
+if __name__=="__main__":
+    start_time=time.time()
+    args        = readArguments()
+    msname      = args["filename"]
+    ntsol       = args["ntsol"]
+    for ms in msname:
+        print "Finding time-covariance weights for: %s"%ms
+        covweights=CovWeights(MSName=ms,ntsol=ntsol)
+        coefficients=covweights.FindWeights(tcorr=0)
+    print "Total runtime: %f min"%((time.time()-start_time)/60.)
--- PngCutoutFromFits.py
+++ PngCutoutFromFits.py
@@ -13,6 +13,9 @@ import glob
 from pyrap.images import image
 import warnings
 warnings.filterwarnings("ignore")
+import pylab
+from astropy.io import fits
+import matplotlib as plt
 
 def readArguments():
     # create parser
@@ -24,22 +27,29 @@ def readArguments():
     parser.add_argument("--vmin",type=float,default=-2,help="Minimum value for display, in units of rms. Default -2.",required=False)
     parser.add_argument("--vmax",type=float,default=40,help="Maximum value for display, in units of rms. Default 40.",required=False)
     parser.add_argument("--size",metavar="arcmin",type=float,default=5,help="Size of cutout, in arcmin. Default is 5")
-    parser.add_argument("--noise",metavar="all/first",choices=["all","first"],type=str,default="first",help="Set of images over which to estimate noise. Default is \"first\", only the first image in the list.",required=False)
-    parser.add_argument("--pol",metavar="I/Q/U/V",choices=["I","Q","U","V"],type=str,default="I",help="Polarisation to choose, in case of polarimetric fits file. Default is \"I\".",required=False)
-    parser.add_argument("--chan",metavar="channel",type=int,default=0,help="Frequency channel to choose, in case of mutli-frequency image cube. Default is 0.",required=False)
+    parser.add_argument("--noise",metavar="all/first",choices=["all","first"],type=str,default="first",\
+                        help="Set of images over which to estimate noise. Default is \"first\", only the first image in the list.",required=False)
+    parser.add_argument("--pol",metavar="I/Q/U/V",choices=["I","Q","U","V"],type=str,default="I",\
+                        help="Polarisation to choose, in case of polarimetric fits file. Default is \"I\".",required=False)
+    parser.add_argument("--chan",metavar="channel",type=int,default=0,\
+                        help="Frequency channel to choose, in case of mutli-frequency image cube. Default is 0."\
+                        ,required=False)
+    parser.add_argument("--histogram",help="Add histogram of the pixel distribution below the image",required=False,default=False,action="store_true")
+    parser.add_argument("--invert",help="Invert colourmap",required=False,default=False,action="store_true")
+    parser.add_argument("--normalise",help="Normalise average pixel value to 1 in all images",required=False,default=False,action="store_true")
     # parse
     args=parser.parse_args()
     return vars(args)
 
 
-def MakeCutout(fitsfiles,RA,Dec,xDegrees=0.1,yDegrees=0.1,NSigmaVmax=10,NSigmaVmin=-10,outname=None,SetVminToAverageNoise="first",verb=0,pol=0,chan=0):
+def MakeCutout(fitsfiles,RA,Dec,xDegrees=0.1,yDegrees=0.1,NSigmaVmax=10,NSigmaVmin=-10,outname=None,\
+               SetVminToAverageNoise="first",verb=0,pol=0,chan=0,histogram=0,invertCmap=False):
     RAdeg  = HHMMSStoDegrees(RA)*15. # this converts RA from hours to degrees
     Decdeg = HHMMSStoDegrees(Dec)
     # find vmin, vmax
     if SetVminToAverageNoise=="all":
         stdarray=[]
         for ffile in fitsfiles:
-            print "here"
             im=image(ffile)
             d=im.getdata()
             ind=np.int64(np.random.rand(10000)*d.size)
@@ -49,7 +59,7 @@ def MakeCutout(fitsfiles,RA,Dec,xDegrees=0.1,yDegrees=0.1,NSigmaVmax=10,NSigmaVm
             stdarray.append(std)
 #            vmin=NSigmaVmin*std
 #            vmax=NSigmaVmax*std
-            im.close()
+#            im.close()
         vmin=NSigmaVmin*np.mean(stdarray)
         vmax=NSigmaVmax*np.mean(stdarray)        
     else:
@@ -64,22 +74,61 @@ def MakeCutout(fitsfiles,RA,Dec,xDegrees=0.1,yDegrees=0.1,NSigmaVmax=10,NSigmaVm
         vmin=NSigmaVmin*std
         vmax=NSigmaVmax*std
 
+    if histogram:
+        if verb: print "Initialising overall histogram bounds"
+        blarg=[]
+        blorg=[]
+        narray=[]
+        binsarray=[]
+        for ffile in fitsfiles:
+            hdulist=fits.open(ffile)
+            pixelvals=hdulist[0].data[chan,pol].ravel()
+            n,bins,_=pylab.hist(pixelvals,bins=np.sqrt(len(pixelvals)).astype(np.int),normed=True)
+            blarg.append([np.max(pixelvals),np.min(pixelvals)])
+            blorg.append(np.max(n))
+            hdulist.close()
+            narray.append(n)
+            binsarray.append(bins)
+        minval=np.min(blarg)
+        maxval=np.max(blarg)
+        ymax=1.1*np.max(blorg)
+        pylab.clf()
+        if verb: print "Initialisation complete: making cutouts"
+    i=0
+    if histogram:
+        ytickvals=np.arange(6)/5.*np.log(np.max(narray))
+        ytickvals=(10*ytickvals).astype(np.int)/10.
     for ffile in fitsfiles:
-        temp = FITSFigure(ffile,slices=[chan,pol])
-        temp.show_grayscale(vmin=vmin,vmax=vmax)
+        test=pylab.figure(figsize=(8,8))
+        ax=pylab.subplot(111)
+        temp = FITSFigure(ffile,slices=[chan,pol],figure=test,subplot=[0,0,1,1])
+        temp.show_grayscale(vmin=vmin,vmax=vmax,invert=invertCmap)
         temp.recenter(RAdeg,Decdeg,width=yDegrees,height=xDegrees)
-        # do grid
-#        temp.add_grid()
-#        temp.grid.set_alpha(0.8)
-#        temp.grid.set_color("red")
-        # do colorbar
         temp.add_colorbar()
         temp.colorbar.set_width(0.2)
         temp.colorbar.set_location("right")
-        temp.save(ffile+"."+RA+"."+Dec+".png")
-        print "Created new cutout: %s"%ffile+"."+RA+"."+Dec+".png"
+        if histogram:
+#            rect=pylab.Rectangle((0.2,0.2),.6,.6,facecolor="red",edgecolor="black")#,transform=ax.transFigure)
+#            ax.add_patch(rect)
+            rectsuperplot=pylab.axes([-0.19,0.79,0.515,0.18])
+            rectsuperplot.tick_params(labelsize=0)
+            rectsuperplot.tick_params(size=0)
+            rectsuperplot.add_patch(plt.patches.Rectangle((0,0),1,1,facecolor="white",edgecolor="black"))
+            histsuperplot=pylab.axes([-.1,0.85,0.4,0.1])
+            logarray=np.log(narray[i])
+            logarray[logarray<0]=0
+            pylab.bar(binsarray[i][0:-1],logarray,align="edge",width=(binsarray[i][1]-binsarray[i][0]),facecolor="white")
+            pylab.xlim((-std*2,std*2))
+            pylab.yticks(ytickvals)
+            pylab.ylabel("log(count)")            
+            pylab.xlabel("Flux")
+            i=i+1
+#        temp.save(ffile+"."+RA+"."+Dec+".png")
+        temp.save(ffile+".png")
+        print "Created new cutout: %s"%ffile+".png"
         temp.close()
-
+    print "Done, thank you for using this script"
+            
 def HHMMSStoDegrees(HHMMSS):
    # convert HHMMSS string to angular value float
    HH,MM,SS=np.array(HHMMSS.split(":")).astype(float)
@@ -100,12 +149,27 @@ if __name__=="__main__":
     noisearg=args["noise"]
     chan=args["chan"]
     pol=args["pol"]
-    print pol
+    hist=args["histogram"]
+    cmapInv=args["invert"]
+    normalise=args["normalise"]
     for i,j in enumerate(["I","Q","U","V"]):
         if pol==j:
             polnum=i
     if verb:
-        print "Making cutouts of size %2.1f' at (RA=%s,Dec=%s) for the following files:"%(size,RA,Dec)
-        for i in fitsfiles:
-            print i
-    MakeCutout(fitsfiles,RA,Dec,xDegrees=size/60.,yDegrees=size/60.,NSigmaVmin=vmin,NSigmaVmax=vmax,outname=None,verb=verb,SetVminToAverageNoise=noisearg,pol=polnum,chan=chan)
+        print "Making cutouts of size %2.1f' centred at (RA=%s,Dec=%s) for the following files:"%(size,RA,Dec)
+    if normalise:
+        normfiles=[]
+    for i in fitsfiles:
+        if normalise:
+            if verb: "Making normalised fits images, then cutouts"
+            data,header=fits.getdata(i,header=True)
+            data=data/np.mean(data,dtype=np.float64)
+            fits.writeto(i[:-4]+"normalised.fits",data,header,clobber=True)
+            normfiles.append(i[:-4]+"normalised.fits")
+        print i
+    if normalise:
+        MakeCutout(normfiles,RA,Dec,xDegrees=size/60.,yDegrees=size/60.,NSigmaVmin=vmin,NSigmaVmax=vmax,outname=None,\
+                   verb=verb,SetVminToAverageNoise=noisearg,pol=polnum,chan=chan,histogram=hist,invertCmap=cmapInv)
+    else:
+        MakeCutout(fitsfiles,RA,Dec,xDegrees=size/60.,yDegrees=size/60.,NSigmaVmin=vmin,NSigmaVmax=vmax,outname=None,\
+                   verb=verb,SetVminToAverageNoise=noisearg,pol=polnum,chan=chan,histogram=hist,invertCmap=cmapInv)
