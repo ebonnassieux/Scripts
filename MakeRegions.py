aa414601de145598f3407f103052193a03f75de4
Etienne Bonnassieux <etienne.bonnassieux@str957-287.personale.dir.unibo.it>
2020-06-16 14:13:30 +0200

    fixed bug with radius

--- MakeRegions.py
+++ MakeRegions.py
@@ -22,7 +22,6 @@ def readArguments():
 def findPrimaryBeam(hdul,fitstype="MIRIAD"):
     # function which takes hdul and returns size of restoring beam in arcsec. Assumes normalised gaussian with 2 fwhm params.
     test_list=hdul[0].header["HISTORY"]
-    print hdul[0].header
     if fitstype=="MIRIAD":
         # read header assuming miriad header format. FWHM given in units of arcsec
         fwhm = np.array(list(filter(lambda x: "fwhm" in x, hdul[0].header["HISTORY"]))[0].split("=")[1].split(",")).astype(float)
@@ -40,13 +39,14 @@ def PixelCoords(hdul,regfilename):
     X,Y=np.meshgrid(x,y)
     ra, dec = w.all_pix2world(X, Y, 1)
     # now read the info from the region file
-    regions=read_ds9(regfilename)
+    regions=read_ds9(regfilename,errors='warn')
     # assume regions are all rectangles. Do not assume anything on rotation.
     ravals=np.array([])
     decvals=np.array([])
     xcovals=np.array([])
     ycovals=np.array([])
     for reg in regions:
+        print reg
         ang=Angle(reg.angle,u.radian).degree
         centRA,centDec=(reg.center.ra.degree,reg.center.dec.degree)
         deltara=Angle(reg.width,u.radian).degree
@@ -99,7 +99,7 @@ def writeRegions(filename,pixelcoords,worldcoords,primarybeam,dirname="Regions")
         f.write("fk5\n")
         ra=Angle(worldcoords[i,0],u.degree).to_string(sep=":",unit=u.hour)
         dec=Angle(worldcoords[i,1],u.degree).to_string(sep=":",unit=u.degree)
-        f.write("ellipse(%s,%s,%f\",%f\",%f)"%(ra,dec,primarybeam[0][0],primarybeam[0][1],primarybeam[1]))
+        f.write("ellipse(%s,%s,%f\",%f\",%f)"%(ra,dec,..5*primarybeam[0][0],.5*primarybeam[0][1],primarybeam[1]))
         debugf.write("\nellipse(%s,%s,%f\",%f\",%f)"%(ra,dec,primarybeam[0][0]/25,primarybeam[0][1]/20,primarybeam[1]))
         f.close()
     debugf.close()
