c2a2ca146701a7a9bdfd342a9e3663592683c28a
ebonnassieux <etienne.bonnassieux@physik.uni-wuerzburg.de>
2024-06-13 17:50:59 +0200

    Add files via upload

--- FixPolarisationSPAM.py
+++ FixPolarisationSPAM.py
@@ -1,6 +1,7 @@
 from casacore.tables import table
 import numpy as np
 import os
+import argparse
 
 def readArguments():
     parser=argparse.ArgumentParser("Fix the incorrect polarisation labeling of SPAM-calibrated GMRT datasets from RR to I")
@@ -8,17 +9,18 @@ def readArguments():
                         required=False,action="store_true")
     parser.add_argument("--filename",  type=str,   help="Name of the measurement set to "+\
                         "be corrected.", required=True,nargs="+")
+    args=parser.parse_args()
+    return vars(args)
 
 def FixDataset(filename,verb):
     outfilename = filename.replace('RR','I')
-    os.sys('cp -r %s %s'filename,outfilename)
+    os.sys('cp -r %s %s'%(filename,outfilename))
     
     t=tables.table(outfilename+"/POLARIZATION", readonly=False,ack=verb)
     t.putcol("CORR_TYPE",np.array([[1]],dtype=np.int32))
     t.close()
 
 if __name__=="__main__":
-    start_time     = time.time()
     # read arguments
     args           = readArguments()
     verb           = args["verbose"]
